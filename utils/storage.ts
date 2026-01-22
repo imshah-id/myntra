ba2824e0d6b1fb378b668785385ae13eff6e314c
Shahid Shamim Shah <shahidshamimshah3@gmail.com>
2026-01-22 19:45:50 +0530

    Myntra: utils: update local storage logic to handle web

--- utils/storage.ts
+++ utils/storage.ts
@@ -1,24 +1,52 @@
+import { Platform } from "react-native";
 import * as SecureStore from "expo-secure-store";
+
+const isWeb = Platform.OS === "web";
+
+const setItem = async (key: string, value: string) => {
+  if (isWeb) {
+    localStorage.setItem(key, value);
+  } else {
+    await SecureStore.setItemAsync(key, value);
+  }
+};
+
+const getItem = async (key: string) => {
+  if (isWeb) {
+    return localStorage.getItem(key);
+  } else {
+    return await SecureStore.getItemAsync(key);
+  }
+};
+
+const removeItem = async (key: string) => {
+  if (isWeb) {
+    localStorage.removeItem(key);
+  } else {
+    await SecureStore.deleteItemAsync(key);
+  }
+};
 
 export const saveUserData = async (
   _id: string,
   name: string,
-  email: string
+  email: string,
 ) => {
-  await SecureStore.setItemAsync("userid", _id);
-  await SecureStore.setItemAsync("userName", name);
-  await SecureStore.setItemAsync("userEmail", email);
+  await setItem("userid", _id);
+  await setItem("userName", name);
+  await setItem("userEmail", email);
 };
 
 export const getUserData = async () => {
-  const _id = await SecureStore.getItemAsync("userid");
-  const name = await SecureStore.getItemAsync("userName");
-  const email = await SecureStore.getItemAsync("userEmail");
+  const _id = await getItem("userid");
+  const name = await getItem("userName");
+  const email = await getItem("userEmail");
+
   return { _id, name, email };
 };
 
 export const clearUserData = async () => {
-  await SecureStore.deleteItemAsync("userid");
-  await SecureStore.deleteItemAsync("userName");
-  await SecureStore.deleteItemAsync("userEmail");
+  await removeItem("userid");
+  await removeItem("userName");
+  await removeItem("userEmail");
 };
